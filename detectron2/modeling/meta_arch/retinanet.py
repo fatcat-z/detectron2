88635bd46258c725af00e963e4f2b636c39e7a2e
fatcat-z <jiz@microsoft.com>
2021-03-10 14:25:32 +0800

    Support to export.

--- detectron2/modeling/meta_arch/retinanet.py
+++ detectron2/modeling/meta_arch/retinanet.py
@@ -229,7 +229,8 @@ class RetinaNet(nn.Module):
         vis_name = f"Top: GT bounding boxes; Bottom: {max_boxes} Highest Scoring Results"
         storage.put_image(vis_name, vis_img)
 
-    def forward(self, batched_inputs: Tuple[Dict[str, Tensor]]):
+    def forward(self, input_image: Tensor, orig_input: Tensor):
+    # def forward(self, batched_inputs: Tuple[Dict[str, Tensor]]):
         """
         Args:
             batched_inputs: a list, batched outputs of :class:`DatasetMapper` .
@@ -248,6 +249,23 @@ class RetinaNet(nn.Module):
                 mapping from a named loss to a tensor storing the loss. Used during training only.
             in inference, the standard output format, described in :doc:`/tutorials/models`.
         """
+
+        input_height, input_width = orig_input.shape[:2]
+
+        if (isinstance(input_height, torch.Tensor)):
+            input_height = input_height.item()
+
+        if (isinstance(input_width, torch.Tensor)):
+            input_width = input_width.item()
+
+        batched_inputs = [{'image': input_image, 'height': input_height, 'width': input_width}]
+
+        # if (isinstance(batched_inputs[0]['height'], torch.Tensor)):
+        #     batched_inputs[0]['height'] = batched_inputs[0]['height'][0].item()
+
+        # if (isinstance(batched_inputs[0]['width'], torch.Tensor)):
+        #     batched_inputs[0]['width'] = batched_inputs[0]['width'][0].item()
+
         images = self.preprocess_image(batched_inputs)
         features = self.backbone(images.tensor)
         features = [features[f] for f in self.head_in_features]
@@ -283,10 +301,15 @@ class RetinaNet(nn.Module):
             for results_per_image, input_per_image, image_size in zip(
                 results, batched_inputs, images.image_sizes
             ):
-                height = input_per_image.get("height", image_size[0])
-                width = input_per_image.get("width", image_size[1])
+                height = input_per_image.get("height", input_height)
+                width = input_per_image.get("width", input_width)            
+                # height = input_per_image.get("height", image_size[0])
+                # width = input_per_image.get("width", image_size[1])
                 r = detector_postprocess(results_per_image, height, width)
-                processed_results.append({"instances": r})
+                # processed_results.append({"instances": r.__str__()})
+                # processed_results.append(torch.tensor(r.image_size[0]))
+                processed_results.append((r.scores, torch.tensor(r.image_size[0])))
+
             return processed_results
 
     def losses(self, anchors, pred_logits, gt_labels, pred_anchor_deltas, gt_boxes):
